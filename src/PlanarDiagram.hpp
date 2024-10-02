6d4578df5e1b3458d3d2b61cece81b8b67b74d9a
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-10-02 17:11:37 -0400

    Preparing for changing PDCode interface to quadruples.

--- src/PlanarDiagram.hpp
+++ src/PlanarDiagram.hpp
@@ -580,118 +580,6 @@ namespace KnotTools
             PD_PRINT("");
         }
         
-        /*! @brief Construction from PD codes and handedness of crossings.
-         *
-         *  @param pd_codes_ Integer array of length `5 * crossing_count_`.
-         *  There is one 5-tuple for each crossing.
-         *  The first 4 entries in each 5-tuple store the actual PD code.
-         *  The last entry gives the handedness of the crossing:
-         *    >  0 for a right-handed crossing
-         *    <= 0 for a left-handed crossing
-         *
-         *  @param crossing_count_ Number of crossings in the diagram.
-         *
-         *  @param unlink_count_ Number of unlinks in the diagram. (This is necessary as PD codes cannot track trivial unlinks.
-         *
-         */
-        
-        template<typename ExtInt, typename ExtInt2, typename ExtInt3>
-        PlanarDiagram(
-            cptr<ExtInt> pd_codes_,
-            const ExtInt2 crossing_count_,
-            const ExtInt3 unlink_count_
-        )
-        :   PlanarDiagram(
-                int_cast<Int>(crossing_count_),
-                int_cast<Int>(unlink_count_)
-            )
-        {
-            static_assert( IntQ<ExtInt>, "" );
-            
-            if( crossing_count_ <= 0 )
-            {
-                return ;
-            }
-            
-            A_cross.Fill(-1);
-
-            // The maximally allowed arc index.
-            const Int max_a = 2 * crossing_count_ - 1;
-            
-            for( Int c = 0; c < crossing_count; ++c )
-            {
-                Int X [5];
-                copy_buffer<5>( &pd_codes_[5*c], &X[0] );
-                
-                if( (X[0] > max_a) || (X[1] > max_a) || (X[2] > max_a) || (X[3] > max_a) )
-                {
-                    eprint( ClassName()+"(): There is a pd code entry that is greater than 2 * number of crosssings - 1." );
-                    return;
-                }
-                
-                bool righthandedQ = (X[4] > 0);
-                
-                Int C [2][2];
-                
-                if( righthandedQ )
-                {
-                    C_state[c] = CrossingState::RightHanded;
-                    
-                    /*
-                     *    X[2]           X[1]
-                     *          ^     ^
-                     *           \   /
-                     *            \ /
-                     *             / <--- c
-                     *            ^ ^
-                     *           /   \
-                     *          /     \
-                     *    X[3]           X[0]
-                     */
-                    
-                    A_cross(X[0],Head) = c;
-                    A_cross(X[1],Tail) = c;
-                    A_cross(X[2],Tail) = c;
-                    A_cross(X[3],Head) = c;
-
-                    C[Out][Left ] = X[2];
-                    C[Out][Right] = X[1];
-                    C[In ][Left ] = X[3];
-                    C[In ][Right] = X[0];
-                }
-                else // left-handed
-                {
-                    C_state[c] = CrossingState::LeftHanded;
-                    
-                    /*
-                     *    X[3]           X[2]
-                     *          ^     ^
-                     *           \   /
-                     *            \ /
-                     *             \ <--- c
-                     *            ^ ^
-                     *           /   \
-                     *          /     \
-                     *    X[0]           X[1]
-                     */
-                    
-                    A_cross(X[0],Head) = c;
-                    A_cross(X[1],Head) = c;
-                    A_cross(X[2],Tail) = c;
-                    A_cross(X[3],Tail) = c;
-                    
-                    C[Out][Left ] = X[3];
-                    C[Out][Right] = X[2];
-                    C[In ][Left ] = X[0];
-                    C[In ][Right] = X[1];
-                }
-                
-                copy_buffer<4>( &C[0][0], C_arcs.data(c) );
-            }
-            
-            fill_buffer( A_state.data(), ArcState::Active, arc_count );
-        }
-        
         
         /*!
          * @brief Returns the number of trivial unlinks in the diagram, i.e., unknots that do not share any crossings with other link components.
@@ -2055,8 +1943,7 @@ namespace KnotTools
             }
             
             return writhe;
-        }
-
+        }        
         
     public:
         
--- src/PlanarDiagram/PDCode.hpp
+++ src/PlanarDiagram/PDCode.hpp
@@ -1,5 +1,121 @@
 public:
 
+/*! @brief Construction from PD codes and handedness of crossings.
+ *
+ *  @param pd_codes_ Integer array of length `5 * crossing_count_`.
+ *  There is one 5-tuple for each crossing.
+ *  The first 4 entries in each 5-tuple store the actual PD code.
+ *  The last entry gives the handedness of the crossing:
+ *    >  0 for a right-handed crossing
+ *    <= 0 for a left-handed crossing
+ *
+ *  @param crossing_count_ Number of crossings in the diagram.
+ *
+ *  @param unlink_count_ Number of unlinks in the diagram. (This is necessary as PD codes cannot track trivial unlinks.
+ *
+ */
+
+template<typename ExtInt, typename ExtInt2, typename ExtInt3>
+PlanarDiagram(
+    cptr<ExtInt> pd_codes_,
+    const ExtInt2 crossing_count_,
+    const ExtInt3 unlink_count_
+)
+:   PlanarDiagram(
+        int_cast<Int>(crossing_count_),
+        int_cast<Int>(unlink_count_)
+    )
+{
+    static_assert( IntQ<ExtInt>, "" );
+    
+    if( crossing_count_ <= 0 )
+    {
+        return ;
+    }
+    
+    A_cross.Fill(-1);
+
+    // The maximally allowed arc index.
+    const Int max_a = 2 * crossing_count_ - 1;
+    
+    for( Int c = 0; c < crossing_count; ++c )
+    {
+        dump(c);
+        
+        Int X [5];
+        copy_buffer<5>( &pd_codes_[5*c], &X[0] );
+        
+        print( ArrayToString( &X[0], {5} ) );
+        
+        if( (X[0] > max_a) || (X[1] > max_a) || (X[2] > max_a) || (X[3] > max_a) )
+        {
+            eprint( ClassName()+"(): There is a pd code entry that is greater than 2 * number of crosssings - 1." );
+            return;
+        }
+        
+        bool righthandedQ = (X[4] > 0);
+        
+        Int C [2][2];
+        
+        if( righthandedQ )
+        {
+            C_state[c] = CrossingState::RightHanded;
+            
+            /*
+             *    X[2]           X[1]
+             *          ^     ^
+             *           \   /
+             *            \ /
+             *             / <--- c
+             *            ^ ^
+             *           /   \
+             *          /     \
+             *    X[3]           X[0]
+             */
+            
+            A_cross(X[0],Head) = c;
+            A_cross(X[1],Tail) = c;
+            A_cross(X[2],Tail) = c;
+            A_cross(X[3],Head) = c;
+
+            C[Out][Left ] = X[2];
+            C[Out][Right] = X[1];
+            C[In ][Left ] = X[3];
+            C[In ][Right] = X[0];
+        }
+        else // left-handed
+        {
+            C_state[c] = CrossingState::LeftHanded;
+            
+            /*
+             *    X[3]           X[2]
+             *          ^     ^
+             *           \   /
+             *            \ /
+             *             \ <--- c
+             *            ^ ^
+             *           /   \
+             *          /     \
+             *    X[0]           X[1]
+             */
+            
+            A_cross(X[0],Head) = c;
+            A_cross(X[1],Head) = c;
+            A_cross(X[2],Tail) = c;
+            A_cross(X[3],Tail) = c;
+            
+            C[Out][Left ] = X[3];
+            C[Out][Right] = X[2];
+            C[In ][Left ] = X[0];
+            C[In ][Right] = X[1];
+        }
+        
+        copy_buffer<4>( &C[0][0], C_arcs.data(c) );
+    }
+    
+    fill_buffer( A_state.data(), ArcState::Active, arc_count );
+}
+
 /*!
  * @brief Returns the pd-codes of the crossing as Tensor2 object.
  *
@@ -252,4 +368,92 @@ Tensor2<Int,Int> PDCode() const
     ptoc( ClassName()+"::PDCode");
     
     return pdcode;
+}
+
+
+
+private:
+
+CrossingState PDHandedness( mptr<Int> X )
+{
+    const Int i = X[0];
+    const Int j = X[1];
+    const Int k = X[2];
+    const Int l = X[3];
+    
+    // This is what we know so far:
+    //
+    //      l \     ^ k
+    //         \   /
+    //          \ /
+    //           \
+    //          / \
+    //         /   \
+    //      i /     \ j
+    //
+
+    // Generically, we should have l = j + 1 or j = l + 1.
+    // But of course, we also have to treat the edge case where
+    // j and l differ by more than one due to the wrap-around at the end
+    // of a connected component.
+    
+    // I "stole" this pretty neat code snippet from the KnotTheory Mathematica package by Dor Bar-Natan.
+    
+    if( (i == j) || (k == l) || (j == l + 1) || (l > j + 1) )
+    {
+        // These are right-handed:
+        //
+        //       O       O            O-------O
+        //      l \     ^ k          l \     ^ k
+        //         \   /                \   /
+        //          \ /                  \ /
+        //           \                    \
+        //          / \                  / \
+        //         /   \                /   \
+        //      i /     v j          i /     v j
+        //       O---<---O            O       O
+        //
+        //       O       O            O       O
+        //      l \     ^ k     j + x  \     ^ k
+        //         \   /                \   /
+        //          \ /                  \ /
+        //           \                    \
+        //          / \                  / \
+        //         /   \                /   \
+        //      i /     v l + 1     i  /     v j
+        //       O       O            O       O
+        
+        return CrossingState::RightHanded;
+    }
+    else if( (i == l) || (j == k) || (l == j + 1) || (j > l + 1) )
+    {
+        // These are left-handed:
+        //
+        //       O       O            O       O
+        //      l|^     ^ k          l ^     ^|k
+        //       | \   /                \   / |
+        //       |  \ /                  \ /  |
+        //       |   \                    \   |
+        //       |  / \                  / \  |
+        //       | /   \                /   \ |
+        //      i|/     \ j          i /     \|j
+        //       O       O            O       O
+        //
+        //       O       O            O       O
+        //    j+1 ^     ^ k         l  ^     ^ k
+        //         \   /                \   /
+        //          \ /                  \ /
+        //           \                    \
+        //          / \                  / \
+        //         /   \                /   \
+        //      i /     \ j         i  /     \ l + x
+        //       O       O            O       O
+        //
+        return CrossingState::LeftHanded;
+    }
+    else
+    {
+        eprint(ClassName() +"::PDHandedness: Handedness of {" + ToString(i) + "," + ToString(i) + "," + ToString(j) + "," + ToString(k) + "," + ToString(l) + "} could not be determined. Make sure that consecutive arcs on each component have consecutive labels (except the wrap-around, of course).");
+        return CrossingState::Inactive;
+    }
 }
