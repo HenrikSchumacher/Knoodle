b68f9744e97d7fed3d1065a680d3c56889d54173
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-11-30 00:08:51 -0500

    Moved box intersection check to AABB.

--- src/Link_2D/FindIntersections_DFS.hpp
+++ src/Link_2D/FindIntersections_DFS.hpp
@@ -2,35 +2,10 @@ public:
 
     bool BoxesIntersectQ( const Int i, const Int j ) const
     {
-        if constexpr ( VectorizableQ<Real> )
-        {
-            // TODO: Not sure whether this makes things faster or slower...
-            vec_T<4,Real> a {
-                box_coords(i,0,0), box_coords(j,0,0), box_coords(i,1,0), box_coords(j,1,0)
-            };
-            
-            vec_T<4,Real> b {
-                box_coords(j,0,1), box_coords(i,0,1), box_coords(j,1,1), box_coords(i,1,1)
-            };
-            
-            return __builtin_reduce_and( a <= b );
-        }
-        else
-        {
-            return
-                (i == j)
-                ? true
-                : (
-                   ( box_coords(i,0,0) <= box_coords(j,0,1) )
-                   &&
-                   ( box_coords(i,0,1) >= box_coords(j,0,0) )
-                   &&
-                   ( box_coords(i,1,0) <= box_coords(j,1,1) )
-                   &&
-                   ( box_coords(i,1,1) >= box_coords(j,1,0) )
-                );
-        }
+        return T.BoxesIntersectQ( box_coords.data(i), box_coords.data(j) );
     }
+
+
 
     void ComputeEdgeIntersection( const Int k, const Int l )
     {
@@ -44,14 +19,6 @@ public:
             const Tiny::Matrix<2,3,Real,Int> y { edge_coords.data(l) };
 
             const bool intersectingQ = LineSegmentsIntersectQ_Kahan( x[0], x[1], y[0], y[1] );
-            
-//            const Vector3_T x[2] = { edge_coords.data(k,0), edge_coords.data(k,1) };
-//            
-//            const Vector3_T y[2] = { edge_coords.data(l,0), edge_coords.data(l,1) };
-
-//            const bool intersectingQ = LineSegmentsIntersectQ_Kahan(
-//                    &x[0][0], &x[1][0], &y[0][0], &y[1][0]
-//            );
 
 
             if( intersectingQ )
@@ -151,138 +118,25 @@ public:
         }
     }
 
-//    void ComputeEdgeIntersection( const Int k, const Int l )
-//    {
-//        // Only check for intersection of edge k and l if they are not equal and not direct neighbors.
-//        if( (l != k) && (l != next_edge[k]) && (k != next_edge[l]) )
-//        {
-//            // Get the edge lengths in order to decide what's a "small" determinant.
-//            
-//            const Vector3_T x[2] = { edge_coords.data(k,0), edge_coords.data(k,1) };
-//            
-//            const Vector3_T y[2] = { edge_coords.data(l,0), edge_coords.data(l,1) };
-//            
-//            // TODO: Need more exact intersection test!
-//            
-//            const Vector2_T d { y[0][0] - x[0][0], y[0][1] - x[0][1] };
-//            const Vector2_T u { x[1][0] - x[0][0], x[1][1] - x[0][1] };
-//            const Vector2_T v { y[1][0] - y[0][0], y[1][1] - y[0][1] };
-//
-//            const Real det = Det_Kahan( u, v );
-//            
-//            Real t[2];
-//
-//            bool intersecting;
-//
-//            if( det * det > eps * Dot_Kahan(u,u) * Dot_Kahan(v,v) )
-//            {
-//                const Real det_inv = static_cast<Real>(1) / det;
-//                
-//                t[0] = Det_Kahan( d, v ) * det_inv;
-//                t[1] = Det_Kahan( d, u ) * det_inv;
-//                
-//                intersecting = (t[0] > - eps) && (t[0] < big_one) && (t[1] > - eps) && (t[1] < big_one);
-//            }
-//            else
-//            {
-//                intersecting = false;
-//
-//                intersections_nontransversal++;
-//            }
-//
-//            if( intersecting )
-//            {
-//                
-//                // Compute heights at the intersection.
-//                const Real h[2] = {
-//                    x[0][2] * (one - t[0]) + t[0] * x[1][2],
-//                    y[0][2] * (one - t[1]) + t[1] * y[1][2]
-//                };
-//                
-//                // Tell edges k and l that they contain an additional crossing.
-//                edge_ptr[k+1]++;
-//                edge_ptr[l+1]++;
-//
-//                if( h[0] < h[1] )
-//                {
-//                    // edge k goes UNDER edge l
-//                    
-//                    intersections.push_back(
-//                        Intersection_T( l, k, t[1], t[0], -Sign(det) )
-//                    );
-//                    
-////      If det > 0, then this looks like this (negative crossing):
-////
-////        v       u
-////         ^     ^
-////          \   /
-////           \ /
-////            \
-////           / \
-////          /   \
-////         /     \
-////        k       l
-////
-////      If det < 0, then this looks like this (positive crossing):
-////
-////        u       v
-////         ^     ^
-////          \   /
-////           \ /
-////            /
-////           / \
-////          /   \
-////         /     \
-////        l       k
-//
-//                }
-//                else if ( h[0] > h[1] )
-//                {
-//                    intersections.push_back(
-//                        Intersection_T( k, l, t[0], t[1], Sign(det) )
-//                    );
-//                    // edge k goes OVER l
-//                    
-////      If det > 0, then this looks like this (positive crossing):
-////
-////        v       u
-////         ^     ^
-////          \   /
-////           \ /
-////            /
-////           / \
-////          /   \
-////         /     \
-////        k       l
-////
-////      If det < 0, then this looks like this (positive crossing):
-////
-////        u       v
-////         ^     ^
-////          \   /
-////           \ /
-////            \
-////           / \
-////          /   \
-////         /     \
-////        l       k
-//                }
-//                else
-//                {
-//                    intersections_3D++;
-//                }
-//            }
-//        }
-//    }
-
 protected:
     
     
-
     void FindIntersectingEdges_DFS()
     {
         ptic(ClassName()+"::FindIntersectingEdges_DFS");
         
+        FindIntersectingEdges_DFS_impl_0();
+        
+//        FindIntersectingEdges_DFS_impl_1();
+        
+//        FindIntersectingEdges_DFS_impl_2();
+
+        ptoc(ClassName()+"::FindIntersectingEdges_DFS");
+        
+    } // FindIntersectingClusters_DFS
+
+    void FindIntersectingEdges_DFS_impl_0()
+    {
         const Int int_node_count = T.InteriorNodeCount();
         
         intersections.clear();
@@ -292,12 +146,10 @@ protected:
         intersections_3D = 0;
         intersections_nontransversal = 0;
         
-        Int i_stack[max_depth] = {};
-        Int j_stack[max_depth] = {};
-        
+        Int stack[max_depth][2];
         Int stack_ptr = 0;
-        i_stack[0] = 0;
-        j_stack[0] = 0;
+        stack[0][0] = 0;
+        stack[0][1] = 0;
         
         edge_ptr.Fill(0);
         
@@ -305,8 +157,12 @@ protected:
         {
             // Pop from stack.
             
-            const Int i = i_stack[stack_ptr];
-            const Int j = j_stack[stack_ptr];
+//            const Int i = i_stack[stack_ptr];
+//            const Int j = j_stack[stack_ptr];
+//            stack_ptr--;
+            
+            const Int i = stack[stack_ptr][0];
+            const Int j = stack[stack_ptr][1];
             stack_ptr--;
             
 
@@ -332,38 +188,38 @@ protected:
                         if( i == j )
                         {
                             //  Creating 3 blockcluster children, since there is one block that is just the mirror of another one.
-                            
+
                             ++stack_ptr;
-                            i_stack[stack_ptr] = L_i;
-                            j_stack[stack_ptr] = R_j;
-                            
+                            stack[stack_ptr][0] = L_i;
+                            stack[stack_ptr][1] = R_j;
+
                             ++stack_ptr;
-                            i_stack[stack_ptr] = R_i;
-                            j_stack[stack_ptr] = R_j;
-                            
+                            stack[stack_ptr][0] = R_i;
+                            stack[stack_ptr][1] = R_j;
+
                             ++stack_ptr;
-                            i_stack[stack_ptr] = L_i;
-                            j_stack[stack_ptr] = L_j;
+                            stack[stack_ptr][0] = L_i;
+                            stack[stack_ptr][1] = L_j;
                         }
                         else
                         {
                             // tie breaker: split both clusters
-                            
+
                             ++stack_ptr;
-                            i_stack[stack_ptr] = R_i;
-                            j_stack[stack_ptr] = R_j;
-                            
+                            stack[stack_ptr][0] = R_i;
+                            stack[stack_ptr][1] = R_j;
+
                             ++stack_ptr;
-                            i_stack[stack_ptr] = L_i;
-                            j_stack[stack_ptr] = R_j;
-                            
+                            stack[stack_ptr][0] = L_i;
+                            stack[stack_ptr][1] = R_j;
+
                             ++stack_ptr;
-                            i_stack[stack_ptr] = R_i;
-                            j_stack[stack_ptr] = L_j;
-                            
+                            stack[stack_ptr][0] = R_i;
+                            stack[stack_ptr][1] = L_j;
+
                             ++stack_ptr;
-                            i_stack[stack_ptr] = L_i;
-                            j_stack[stack_ptr] = L_j;
+                            stack[stack_ptr][0] = L_i;
+                            stack[stack_ptr][1] = L_j;
                         }
                     }
                     else
@@ -371,37 +227,37 @@ protected:
                         // split only larger cluster
                         if( is_interior_i ) // !is_interior_j follows from this.
                         {
-                            ++stack_ptr;
-                            i_stack[stack_ptr] = R_i;
-                            j_stack[stack_ptr] = j;
-                            
                             //split cluster i
+
                             ++stack_ptr;
-                            i_stack[stack_ptr] = L_i;
-                            j_stack[stack_ptr] = j;
+                            stack[stack_ptr][0] = R_i;
+                            stack[stack_ptr][1] = j;
+
+                            ++stack_ptr;
+                            stack[stack_ptr][0] = L_i;
+                            stack[stack_ptr][1] = j;
                         }
                         else //score_i < score_j
                         {
                             //split cluster j
+
                             ++stack_ptr;
-                            i_stack[stack_ptr] = i;
-                            j_stack[stack_ptr] = R_j;
-                            
+                            stack[stack_ptr][0] = i;
+                            stack[stack_ptr][1] = R_j;
+
                             ++stack_ptr;
-                            i_stack[stack_ptr] = i;
-                            j_stack[stack_ptr] = L_j;
+                            stack[stack_ptr][0] = i;
+                            stack[stack_ptr][1] = L_j;
                         }
                     }
                 }
                 else
                 {
-                    ComputeEdgeIntersection( T.Begin(i), T.Begin(j ) );
+                    ComputeEdgeIntersection( T.NodeBegin(i), T.NodeBegin(j ) );
                 }
             }
         }
         
         edge_ptr.Accumulate();
-
-        ptoc(ClassName()+"::FindIntersectingEdges_DFS");
         
-    } // FindIntersectingClusters_DFS
+    } // FindIntersectingClusters_DFS_impl_1
