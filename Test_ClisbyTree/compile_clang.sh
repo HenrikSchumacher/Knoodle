7227b87d8200a3a07771e8c7969753f95a7ececb
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2025-03-09 08:01:26 -0400

    Buggy version. We get line segment-line segment intersections in 3D, which should be impossible with hard sphere constraints.

---
+++ Test_ClisbyTree/compile_clang.sh
@@ -0,0 +1,37 @@
+clang-19                                        \
+    -Wall                                       \
+    -Wextra                                     \
+    -std=c++20                                  \
+    -lc++                                       \
+    -O3                                         \
+    -ffast-math                                 \
+    -flto                                       \
+    -fenable-matrix                             \
+    -pthread                                    \
+    -march=native                               \
+    -mtune=native                               \
+    -I/opt/homebrew/include                     \
+    -L/opt/homebrew/lib                         \
+    -fverbose-asm                               \
+    -o PolyFold                                 \
+    main.cpp                                    \
+    -lboost_program_options                     \
+ 
+#objdump --reloc --line-numbers --demangle --non-verbose --syms PolyFold > PolyFold.asm
+    
+#        -g                                          \
+    
+#
+#                                                \
+#    -Weverything                                \
+#    -Wno-c++98-compat                           \
+#    -Wno-c++98-compat-local-type-template-args  \
+#    -Wno-c++98-compat-pedantic                  \
+#    -Wno-covered-switch-default                 \
+#    -Wno-global-constructors                    \
+#    -Wno-exit-time-destructors                  \
+#    -Wno-padded                                 \
+#                                                \
+#    -Wno-suggest-override                       \
+#    -Wno-zero-as-null-pointer-constant          \
+#                                                \
