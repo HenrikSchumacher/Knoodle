046f6647246ca112babff740dccd210df0f3961e
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2025-03-09 08:02:00 -0400

    Buggy version. We get line segment-line segment intersections in 3D, which should be impossible with hard sphere constraints.

---
+++ Test_ClisbyTree/compile_gcc_kyoto.sh
@@ -0,0 +1,25 @@
+g++                                                 \
+    -m64                                            \
+    -Wall                                           \
+    -Wextra                                         \
+    -Wno-unknown-pragmas                            \
+    -Wno-ignored-qualifiers                         \
+    -std=c++20                                      \
+    -Ofast                                          \
+    -flto                                           \
+    -pthread                                        \
+    -march=native                                   \
+    -mtune=native                                   \
+    -flax-vector-conversions                        \
+    -Wno-psabi                                      \
+    -opolyfold_gcc                                  \
+    main.cpp                                        \
+    -lboost_program_options                         \
+
+
+#    -lboost_system                              \
+#        -lboost_thread                              \
+#    -I/opt/homebrew/include                     \
+#    -L/opt/homebrew/lib                         \
+
+#    -ncpu=apple-m1                              \
