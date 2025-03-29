636d0e52b1904d66adc166bcc7755c4b45774e6c
Jason Cantarella <jason.cantarella@gmail.com>
2025-03-29 14:17:40 -0400

    Added a tweaked build script for Sapelo2 cluster environment

---
+++ PolyFold/compile_clang_sapelo2.sh
@@ -0,0 +1,38 @@
+clang++                                         \
+    -Wall                                       \
+    -Wextra                                     \
+    -std=c++20                                  \
+    -lc++                                       \
+    -Ofast                                      \
+    -ffast-math                                 \
+    -fenable-matrix                             \
+    -pthread                                    \
+    -march=native                               \
+    -mtune=native                               \
+    -I/usr/include/                             \
+    -L/usr/lib64                                \
+    -fverbose-asm                               \
+    -o PolyFold                                 \
+    -DPOLYFOLD_NO_QUATERNIONS                       \
+    -DGIT_VERSION=\"\\\"$(git describe --abbrev=100 --dirty --always --tags)\\\"\" \
+    main.cpp                                    \
+    -lboost_program_options                     \
+ 
+#objdump --reloc --line-numbers --demangle --non-verbose --syms PolyFold > PolyFold.asm
+    
+#        -g                                          \
+    
+#
+#                                                \
+#    -Weverything                                \
+#    -Wno-c++98-compat                           \
+#    -Wno-c++98-compat-local-type-template-args  \
+#    -Wno-c++98-compat-pedantic                  \
+#    -Wno-covered-switch-default                 \
+#    -Wno-global-constructors                    \
+#    -Wno-exit-time-destructors                  \
+#    -Wno-padded                                 \
+#                                                \
+#    -Wno-suggest-override                       \
+#    -Wno-zero-as-null-pointer-constant          \
+#                                                \
